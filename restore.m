c780c3cb71d675854443f8cec9faf4f3ecf878c7
Ssduckk <dlwhdrlf0619@naver.com>
2022-03-04 16:55:25 +0900

    position each class

--- restore.m
+++ restore.m
@@ -1,6 +1,7 @@
 file_input = open('/home/ri/workspace/MATLAB_Labeler/test_restoring.mat');
 labels = fileread('/home/ri/workspace/MATLAB/labels/000026.txt');
-
+fileattrib('/home/ri/workspace/MATLAB_Labeler/test_restoring.mat', '+w');
+LabelData = file_input.gTruth.LabelData;
 labels = split(labels, '---');
 labels_box = split(labels{1}, newline);
 labels_line = jsondecode(labels{2});
@@ -16,13 +17,17 @@ for box_index = 2:length(labels_box)
     y = str2double(box_data{2});
     w = str2double(box_data{5});
     h = str2double(box_data{4});
-    position = [x y w h];
+    position = [x y w h];   
+
+    switch label_name
+        case '승용차'
+            LabelData.Car{1} = position
+        case '사람'
+            LabelData.Person{1} = position
+        case '신호등'
+            LabelData.TrafficLight{1} = position
+    end
 end
-
-fileattrib('/home/ri/workspace/MATLAB_Labeler/test_restoring.mat', '+w');
-LabelData = file_input.gTruth.LabelData;
-
-LabelData.Truck{1} = position
 
 New_Label_Data = LabelData;
 
