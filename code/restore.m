1fbbd74833be572742cd84cf845be1d53e98d4b1
Ssduckk <dlwhdrlf0619@naver.com>
2022-04-03 14:53:35 +0900

    sperate lines but need edit line number

--- code/restore.m
+++ code/restore.m
@@ -1,10 +1,18 @@
-DATASET_ROOT = '/home/ri/workspace/Sample_Result_22.03.11/220209/magok/2022-02-09-19-53-05';
-IMAGE_ROOT = fullfile(DATASET_ROOT, '/image');
-LABEL_ROOT = fullfile(DATASET_ROOT, '/labels');
-DEFINITION_ROOT = '/home/ri/workspace/MATLAB/line1Definition.mat';
-DST_MAT_PATH = '/home/ri/workspace/MATLAB/matfiles';
+DATASET_ROOT = '/home/ri/1st_5000_data/1st_result/220209/magok';
+%IMAGE_ROOT = fullfile(DATASET_ROOT, '/image');
+%LABEL_ROOT = fullfile(DATASET_ROOT, '/label');
+DEFINITION_ROOT = '/home/ri/workspace/MATLAB/line4Definition.mat';
+DST_MAT_PATH = '/home/ri/workspace/MATLAB/';
 
-reconstruct_label_data(IMAGE_ROOT, DEFINITION_ROOT, LABEL_ROOT, DST_MAT_PATH)
+data_path = dir(DATASET_ROOT);
+for input = 3:size(data_path)
+    path = data_path(input).name;
+    data_dir = fullfile(DATASET_ROOT, path);
+    IMAGE_ROOT = fullfile(data_dir, '/image');
+    LABEL_ROOT = fullfile(data_dir, '/label');
+    reconstruct_label_data(IMAGE_ROOT, DEFINITION_ROOT, LABEL_ROOT, DST_MAT_PATH)
+end
+save_original(DATASET_ROOT)
 
 
 function ground_truth = reconstruct_label_data(image_path, definition_path, label_path, dst_mat_path)
@@ -20,7 +28,7 @@ function source_files = list_image_files(image_path)
     data_source = dir(fullfile(image_path, '*.jpg'));
     data = [];
     for i = 1:length(data_source)
-        image_name = strcat(data_source(i).folder, '/', data_source(i).name);
+        image_name = strcat(data_source(i).folder, '/', data_source(i).name)
         data = [data; image_name];
         data = cellstr(data);
     end
@@ -52,7 +60,7 @@ function data_file = read_label_data(image_path, def_path, label_path)
         labels = split(labels, '---');
         labels_box = split(labels{1}, newline);
         
-        for box_index = 2:length(labels_box)
+        for box_index = 1:length(labels_box)
             box_data = split(labels_box(box_index), ',');
             if isempty(box_data{1})
                 continue
@@ -75,12 +83,14 @@ function data_file = read_label_data(image_path, def_path, label_path)
         else
             labels_line = jsondecode(labels{2});
         end
-
+        temp_cell = []
         for line_index = 1:length(labels_line)
             line_list = labels_line{line_index};
             line_name = labels_line{line_index}{1};
             line_point_array = [];
-
+            
+            perfect_line = cell(length(line_list)-1);
+            perfect_line = perfect_line(:, 1);
             for line_point = 2:length(line_list)
                 if length(line_list) == 3
                     continue
@@ -89,11 +99,11 @@ function data_file = read_label_data(image_path, def_path, label_path)
                 y = line_list{line_point}(2);
                 line_point_array = [line_point_array; x y];
             end
-            
-            line_name_number = find(strcmp(label_definition{:, 5}, line_name));
-            line_label_name = label_definition{:, 1}{line_name_number};
-            template_table{:, line_name_number}{file_idx} = [template_table{:, line_name_number}{file_idx}; line_point_array];
+            %라인 분리는 해결했는데 분류가 이상하게 되니 해결해
+            temp_cell = [temp_cell; {line_point_array}]
         end
+        line_name_number = find(strcmp(label_definition{:, 5}, line_name));
+        template_table{:, line_name_number}{file_idx} = temp_cell;
     end
 
     data_file = template_table;
@@ -101,12 +111,23 @@ end
 
 function save_gtruth_file(ground_truth, dst_mat_path)
     dir_name = split(ground_truth.DataSource.Source{1}, '/');
-    dir_name = dir_name{6};
-    if ~exist(fullfile(dst_mat_path, dir_name))
-        mkdir(fullfile(dst_mat_path, dir_name))
+    dir_name = dir_name{9};
+    save_path = fullfile(dst_mat_path, 'matfiles', dir_name);
+    if ~exist(save_path)
+        mkdir(save_path)
     end
     time = split(datestr(datetime('now')), ' ');
     time = time{2};
     output_mat = append(dir_name, '_', string(time));
-    save(fullfile(dst_mat_path, dir_name, output_mat), 'ground_truth')
+    save(fullfile(save_path, output_mat), 'ground_truth')
+end
+
+function save_original(input_data_path)
+    zip_path = fullfile(input_data_path, 'original');
+    input_data_name = split(input_data_path, '/');
+    input_data_name = append(input_data_name{end}, '_origin');
+    if ~exist(zip_path)
+        mkdir(zip_path)
+        zip(fullfile(zip_path, input_data_name), zip_path, input_data_path);
+    end
 end
